6b2ac56e4457886b9ba12375d98128107c4f604f
bakrgit <44190819+bakrgit@users.noreply.github.com>
2022-03-17 04:14:24 +0200

    first commit

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux'
+import categoryReducer from './categoryReducer'
+import brandReducer from './brandReducer'
+import subcategoryReducer from './subcategoryReducer'
+import productsReducer from './productsReducer'
+import authReducer from './authReducer'
+import reviewReducer from './reviewReducer'
+import addToWishListReducer from './wishListReducer'
+export default combineReducers({
+    allCategory: categoryReducer,
+    allBrand: brandReducer,
+    subCategory: subcategoryReducer,
+    allproducts: productsReducer,
+    authReducer: authReducer,
+    reviewReducer: reviewReducer,
+    addToWishListReducer: addToWishListReducer,
+})
